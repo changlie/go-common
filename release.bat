b5b8c168434b387c9d3eaff04b710aaccf50a88c
changlie <583491331@qq.com>
2024-04-24 02:33:02 +0800

    'release script test'

---
+++ release.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set version="v0.1.15"
+set msg="release script test"
+
+git add . && git commit . -m '%msg%' && git push && git tag -a %version% -m "'release %version%'" && git push --tags
--- release.sh
+++ release.sh
@@ -1,11 +1,12 @@
 
 set -xe
 
-ci "type conversion"
+msg=""
+newVersion="v0.1.13"
 
+ci "type conversion"
 git push
 
-newVersion=`nextVersion`
 
 git tag -a $newVersion -m "release $newVersion"
 git push --tags
@@ -13,6 +14,5 @@ git push --tags
 echo "release $newVersion successfully!"
 
 
-# git tag -a v0.1.13 -m "release v0.1.13" && git push --tags
-
-# git add . && git commit . -m "optimize file mod" && git push && git tag -a v0.1.14 -m "release v0.1.14" && git push --tags
+# git tag -a v0.1.13 -m "release " && git push --tags
+# git add . && git commit . -m "" && git push && git tag -a v0.1.14 -m "release v0.1.14" && git push --tags
