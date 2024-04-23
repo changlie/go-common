d827f7bac9a58ea2f4afa83f3d1697a67342b92a
changlie <583491331@qq.com>
2024-04-24 02:36:34 +0800

    'release script test v5'

--- release.bat
+++ release.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set version="v0.1.16"
-set msg="release script test v4"
+set version="v0.1.17"
+set msg="release script test v5"
 
-git add . && git commit . -m '%msg%' && git push && git tag -a %version% -m 'release %version%' && git push --tags
+git add . && git commit . -m '%msg%' && git push && git tag -a %version% -m "'release %version%'" && git push --tags
