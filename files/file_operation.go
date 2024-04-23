4317852428c59800cda24b3922227447d0189a53
changlie <583491331@qq.com>
2024-04-24 01:24:51 +0800

    optimize file mod

--- files/file_operation.go
+++ files/file_operation.go
@@ -1,4 +1,4 @@
-package a
+package files
 
 import (
 	"bufio"
--- release.sh
+++ release.sh
@@ -11,3 +11,8 @@ git tag -a $newVersion -m "release $newVersion"
 git push --tags
 
 echo "release $newVersion successfully!"
+
+
+# git tag -a v0.1.13 -m "release v0.1.13" && git push --tags
+
+# git add . && git commit . -m "optimize file mod" && git tag -a v0.1.13 -m "release v0.1.13" && git push --tags
